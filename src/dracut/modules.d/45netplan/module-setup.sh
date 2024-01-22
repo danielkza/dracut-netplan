f1454c498723eb494e449e0e14f3dabc36d90231
Daniel Miranda <danielkza2@gmail.com>
2024-01-22 11:50:49 +0000

    generate configs at initrd build time

--- src/dracut/modules.d/45netplan/module-setup.sh
+++ src/dracut/modules.d/45netplan/module-setup.sh
@@ -25,47 +25,33 @@ depends() {
   esac
 }
 
-setup_nm_connections() {
-  local nm_conn_dir=/etc/NetworkManager/system-connections
-
-  local _f found=0
-  for _f in "${initdir}/run/NetworkManager/system-connections"/*.nmconnection; do
-    mkdir -p "${initdir}/${nm_conn_dir}/"
-    mv "$_f" "${initdir}/${nm_conn_dir}/"
-
-    found=1
-  done
-
-  [ $found -eq 1 ]
-}
-
-setup_networkd_connections() {
-  local sd_conn_dir=/etc/systemd/network
-
-  local _f found=0
-  for _f in "${initdir}/run/systemd/network"/*.*; do
-    mkdir -p "${initdir}/${sd_conn_dir}/"
-    mv "$_f" "${initdir}/${sd_conn_dir}/"
-
-    found=1
-  done
-
-  [ $found -eq 1 ]
-}
-
 install() {
-  inst_multiple -o -H "/etc/netplan/*.yaml"
-  netplan generate --root-dir "${initdir}"
+  set -x
 
-  renderer=$(netplan get renderer)
-  case "$renderer" in
-  NetworkManager)
-    setup_nm_connections ;;
-  networkd)
-    setup_networkd_connections ;;
-  esac
+  inst_multiple -o -H "/etc/netplan/*.yaml"
+
+  local tmp_root=$(mktemp -d --tmpdir dracut-netplan.XXXX)
+  mkdir -p "${tmp_root}/etc/netplan"
+  cp "${initdir}/etc/netplan"/*.yaml "${tmp_root}/etc/netplan/"
+
+  netplan --debug generate --root-dir "${tmp_root}"
+
+  local src dst
+  while read -r src; do
+    if [ "$(basename "$src")" == netplan-ovs-cleanup.service ]; then
+      continue
+    fi
+
+    dst=$(sed "s!^${tmp_root}/run!${initdir}/etc!" <<< "$src")
+    [ "$src" != "$dst" ] || continue
+
+    mkdir -p $(dirname "$dst")
+    mv "$src" "$dst"
+  done < <(find "${tmp_root}/run" -not -type d)
 
   if [ $? -eq 0 ]; then
     echo "rd.neednet=1" > "${initdir}/etc/cmdline.d/10-netplan-net.conf"
   fi
+
+  set +x
 }
